38d6ba5f2d1f494d9c7ec32964740ac72626824e
wangzw <wang410@gmail.com>
2019-08-30 15:27:31 +0800

    head_12-2 musicplayer version 2 head-12-3 final version

--- head_12_3/src/MiniMusicPlayer3.java
+++ head_12_3/src/MiniMusicPlayer3.java
@@ -9,12 +9,46 @@ public class MiniMusicPlayer3 {
 
     public static void main(String[] args) {
         MiniMusicPlayer3 mini = new MiniMusicPlayer3();
-        mini.go;
+        mini.go();
     }//关闭方法
 
     public void setUpGui() {
         ml = new MyDrawPanel();
-    }
+        f.setContentPane(ml);
+        f.setBounds(30,30,300,300);
+        f.setVisible(true);
+    }//close method
+
+    public void go() {
+        setUpGui();
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            sequencer.addControllerEventListener(ml,new int[] {127});
+            Sequence seq = new Sequence(Sequence.PPQ,4);
+            Track track = seq.createTrack();
+
+            int r = 0;
+            for (int i = 0; i < 60; i+=4){
+                r = (int) ((Math.random()*50)+1);
+                track.add(makeEvent(144,1,r,100,i));
+                track.add(makeEvent(176,1,127,0,i));
+                track.add(makeEvent(128,1,r,100,i+2));
+            }//close iteration
+            sequencer.setSequence(seq);
+            sequencer.setTempoInBPM(120);
+            sequencer.start();
+        }catch (Exception ex) {ex.printStackTrace();}
+    }//close method
+    public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick){
+        MidiEvent event = null;
+        try {
+            ShortMessage a = new ShortMessage();
+            a.setMessage(comd,chan,one,two);
+            event = new MidiEvent(a,tick);
+        }catch (Exception e){}
+        return event;
+    }//close method
 
     class MyDrawPanel extends JPanel implements ControllerEventListener{
         boolean msg = false;
